f2377e385e8e8a0e7dc12eb6363786b85fb9a95d
Tyler Chan <TcFlux@gmail.com>
2020-12-08 12:12:05 -0500

    refactored

--- day3/main.go
+++ day3/main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+var width int
+
 func main() {
 	file, err := os.Open("./input")
 	if err != nil {
@@ -22,53 +24,60 @@ func main() {
 		gridX := strings.Split(scanner.Text(), "")
 		gridY = append(gridY, gridX)
 	}
+	
+	width = len(gridY[0])
 
 	part1(gridY)
-	part2(gridY)
+	// part2(gridY)
+}
+
+func calculateCollisions(grid [][]string, distX, distY int) int {
+	collisions := 0
+	posX := 0
+
+	for posY, gridX := range grid {
+		if posY == 0 {
+			width = len(gridX)
+		} else {
+			posX += distX
+			if posX >= width {
+				posX = posX % width
+			}
+			if gridX[posX] == "#" {
+				collisions += 1
+			}
+		}
+		posY += distY
+	}
+
+	return collisions
 }
 
 func part1(grid [][]string) {
-	collisions := 0
-	posX := 0
-	width := 0
-
-	for posY, gridX := range grid {
-		if posY == 0 {
-			width = len(gridX)
-		} else {
-			posX += 3
-			if posX >= width {
-				posX = posX % width
-			}
-			if gridX[posX] == "#" {
-				collisions += 1
-			}
-		}
-		posY += 1
-	}
+	collisions := calculateCollisions(grid, 3, 1)
 
 	fmt.Printf("The sled would hit %d trees\n", collisions)
 }
 
-func part2() {
-	collisions := 0
-	posX := 0
-	width := 0
+// func part2(grid [][]string) {
+// 	collisions := 0
+// 	posX := 0
+// 	width := 0
 
-	for posY, gridX := range grid {
-		if posY == 0 {
-			width = len(gridX)
-		} else {
-			posX += 3
-			if posX >= width {
-				posX = posX % width
-			}
-			if gridX[posX] == "#" {
-				collisions += 1
-			}
-		}
-		posY += 1
-	}
+// 	for posY, gridX := range grid {
+// 		if posY == 0 {
+// 			width = len(gridX)
+// 		} else {
+// 			posX += 3
+// 			if posX >= width {
+// 				posX = posX % width
+// 			}
+// 			if gridX[posX] == "#" {
+// 				collisions += 1
+// 			}
+// 		}
+// 		posY += 1
+// 	}
 
-	fmt.Printf("The sled would hit %d trees\n", collisions)
-}
+// 	fmt.Printf("The sled would hit %d trees\n", collisions)
+// }
