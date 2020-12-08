c247df959f90f933bd8897322f436950e2eda7b9
Tyler Chan <TcFlux@gmail.com>
2020-12-08 12:46:02 -0500

    stopped being dumb and now done

--- day3/main.go
+++ day3/main.go
@@ -28,56 +28,57 @@ func main() {
 	width = len(gridY[0])
 
 	part1(gridY)
-	// part2(gridY)
+	part2(gridY)
+}
+
+func part1(grid [][]string) {
+	fmt.Printf("The sled would hit %d trees for part 1\n", calculateCollisions(grid, 3, 1))
+}
+
+func part2(grid [][]string) {
+	// index 0 = distX, index 1 = distY
+	paths := [][]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	collisionsList := make([]int, 0)
+
+	for _, coords := range paths {
+		collisions := calculateCollisions(grid, coords[0], coords[1])
+		collisionsList = append(collisionsList, collisions)
+		fmt.Printf("Going right %d, and down %d collides with %d trees\n", coords[0], coords[1], collisions)
+	}
+	fmt.Println(collisionsList)
+	fmt.Printf("The product of the collisions for part 2 would be %d\n", sumArray(collisionsList))
 }
 
 func calculateCollisions(grid [][]string, distX, distY int) int {
 	collisions := 0
 	posX := 0
 
-	for posY, gridX := range grid {
-		if posY == 0 {
-			width = len(gridX)
-		} else {
+	for posY := 0; posY < len(grid); posY += distY {
+		if posY != 0 {
 			posX += distX
 			if posX >= width {
 				posX = posX % width
 			}
-			if gridX[posX] == "#" {
+			if grid[posY][posX] == "#" {
 				collisions += 1
 			}
 		}
-		posY += distY
 	}
 
 	return collisions
 }
 
-func part1(grid [][]string) {
-	collisions := calculateCollisions(grid, 3, 1)
-
-	fmt.Printf("The sled would hit %d trees\n", collisions)
+func sumArray(array []int) int {
+	product := 1
+	for _, val := range array {
+		product *= val
+	}
+	return product
 }
-
-// func part2(grid [][]string) {
-// 	collisions := 0
-// 	posX := 0
-// 	width := 0
-
-// 	for posY, gridX := range grid {
-// 		if posY == 0 {
-// 			width = len(gridX)
-// 		} else {
-// 			posX += 3
-// 			if posX >= width {
-// 				posX = posX % width
-// 			}
-// 			if gridX[posX] == "#" {
-// 				collisions += 1
-// 			}
-// 		}
-// 		posY += 1
-// 	}
-
-// 	fmt.Printf("The sled would hit %d trees\n", collisions)
-// }
