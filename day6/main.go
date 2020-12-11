214bd9f53838a2c6a4eb0b298fa050fa9bda6daf
Tyler Chan <TcFlux@gmail.com>
2020-12-11 10:36:33 -0500

    day 6 part 2

--- day6/main.go
+++ day6/main.go
@@ -58,27 +58,50 @@ func part1(resps [][]string) {
 func part2(resps [][]string) {
 	answerMap := make(map[string]bool)
 	counts := make([]int, 0)
+	groupFirst := true
 	
 	for _, answers := range resps {
 		if len(answers) == 0 {
 			counts = append(counts, len(answerMap))
 			answerMap = make(map[string]bool)
+			groupFirst = true
 		} else {
-			for _, answer := range answers {
-				if _, ok := answerMap[answer]; !ok {
-					answerMap[answer] = true
+			// Don't worry about deleting keys for the first set of answers per group
+			if groupFirst {
+				for _, answer := range answers {
+					if _, ok := answerMap[answer]; !ok {
+						answerMap[answer] = false
+					}
 				}
+			} else {
+				// For subsequent group members set overlapping yes responses to true
+				for _, answer := range answers {
+					if _, ok := answerMap[answer]; ok {
+						answerMap[answer] = true
+					}
+				}
+				// Delete not trues and set trues to false for next round of comparisons
+				for k, v := range answerMap {
+					if v == false {
+						delete(answerMap, k)
+					} else {
+						answerMap[k] = false
+					}
+				}
+			}
+
+			if groupFirst {
+				groupFirst = false
 			}
 		}
 	}
 
 	counts = append(counts, len(answerMap))
-	answerMap = make(map[string]bool)
 
 	sum := 0
 	for _, count := range counts {
 		sum += count
 	}
 
-	fmt.Println("The sum of distinct yes answers per group for part 1 is:", sum)
+	fmt.Println("The sum of answers that the entire group answered yes for in part 2 is:", sum)
 }
